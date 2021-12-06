9f3ec555c9e36f22002ae578edd7f8f218a67b84
Jeffrey Bennett <endymionjkb@gmail.com>
2021-12-05 23:56:41 -0500

    Flailing about trying to resolve conflicts

--- pvt/helpers/src/constants.ts
+++
@@ -1,7 +0,0 @@
-import { BigNumber } from 'ethers';
-
-import { maxUint } from './numbers';
-
-export const MAX_UINT256: BigNumber = maxUint(256);
-
-export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
